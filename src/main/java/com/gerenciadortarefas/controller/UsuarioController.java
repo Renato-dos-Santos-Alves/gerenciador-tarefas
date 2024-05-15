9741eb6f0c5b0db0380e674e150aea67a7d46af7
Renato Alves <renato.santosl@sonda.com>
2024-05-15 13:17:10 -0300

    Criando serviços de atualizar, listar e deletar usuarios

--- src/main/java/com/gerenciadortarefas/controller/UsuarioController.java
+++ src/main/java/com/gerenciadortarefas/controller/UsuarioController.java
@@ -5,10 +5,9 @@ import com.gerenciadortarefas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,6 +18,21 @@ public class UsuarioController {
 
     @PostMapping
     public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario) {
-        return new ResponseEntity<>(usuarioService.salvarUsuario(usuario), HttpStatus.OK);
+        return new ResponseEntity<>(usuarioService.salvarUsuario(usuario), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Usuario> atualizarEsuario(@RequestBody Usuario usuario){
+        return new ResponseEntity<>(usuarioService.atualizarUsuario(usuario), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obtemUsuarios(){
+        return new ResponseEntity<>(usuarioService.obtemUsuarios(), HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public void excluirUsuarios(@RequestBody Usuario usuario){
+        usuarioService.excluirUsuario(usuario);
     }
 }
