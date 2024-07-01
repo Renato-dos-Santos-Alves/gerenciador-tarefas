b89ec881a03e1189275a61b1ba5872a95f4a5433
Renato Alves <renato.santosl@sonda.com>
2024-07-01 15:54:42 -0300

    Criado Obter Tarefas response.

--- src/main/java/com/gerenciadortarefas/controller/GerenciadorTarefasController.java
+++ src/main/java/com/gerenciadortarefas/controller/GerenciadorTarefasController.java
@@ -9,10 +9,7 @@ import com.gerenciadortarefas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
     @RequestMapping ("/gerenciados-tarefas")
@@ -37,4 +34,7 @@ public class GerenciadorTarefasController {
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<>
 }
---
+++ src/main/java/com/gerenciadortarefas/response/ObterTarefasResponse.java
@@ -0,0 +1,4 @@
+package com.gerenciadortarefas.response;
+
+public class ObterTarefasResponse {
+}
